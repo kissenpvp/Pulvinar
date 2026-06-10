ecc56c292769d09eaac2a09970da190b2e039f62
Ivo Quiring <falk.quiring@gmail.com>
2026-06-10 11:45:44 +0200

    Update Upstream (PurpurMC/Purpur)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -52,3 +52,15 @@ fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
         )
     }
 }
+
+gradle.lifecycle.beforeProject {
+    val mcVersion = providers.gradleProperty("mcVersion").get().trim()
+    val pulvinarChannel = providers.gradleProperty("channel").get().trim()
+    val pulvinarBuildNumber = providers.environmentVariable("BUILD_NUMBER").orNull?.trim()?.toInt()
+    val versionString = if (pulvinarBuildNumber == null) {
+        "$mcVersion.local-SNAPSHOT"
+    } else {
+        "$mcVersion.build.$pulvinarBuildNumber-${pulvinarChannel.lowercase()}"
+    }
+    version = versionString
+}
